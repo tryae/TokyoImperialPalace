8657489175c35c3bc4bd0660baa782f13cd7c3e6
Tryae <tryae324@gmail.com>
2025-09-13 18:37:36 -0400

    functional js

--- page.js
+++ page.js
@@ -1,7 +1,29 @@
-const image = document.getElementById('image');
 
-function change() {
-    document.createElement('p', )
+
+window.onload = function () {
+    const button = document.getElementById("button");
+
+    const text = document.getElementById("fact");
+
+    console.log(button);
+
+    const randomFacts = ["It is the main residence of Japan's Emperor.", 'Located on the former site of Edo Castle.',
+    'The inner grounds are rarely open to the public.', 'Surrounded by moats and massive stone walls.', 'Open to visitors on the Emperor’s birthday.']
+
+    function change() {
+        console.log('running');
+        const currentText = text.innerHTML;
+        console.log(currentText)
+
+        const index = Math.floor(Math.random() * 5);
+
+        if (currentText !== randomFacts[index]) {
+            text.innerHTML = randomFacts[index];
+        } else {
+            change();
+        }
+
+    }
+
+    button.addEventListener("click", change);
 }
-
-image.addEventListener('onClick', change);
